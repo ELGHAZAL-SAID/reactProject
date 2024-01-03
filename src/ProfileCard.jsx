3d9ae8527ec064e50001f4c85060acb643bc9503
ELGHAZAL-SAID <said.elghazal@edu.uiz.ac.ma>
2024-01-03 14:36:21 +0100

    some thing u do not have to know about it

--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
- 
-const ProfileCard = ({ name, username, email, about,photo, age, address, hobbies,mobilite,disponibilité }) => {
+
+const ProfileCard = ({ name, username, email, about, photo, age, address, hobbies, mobility, disponibility }) => {
     return (<>
         <div className="profile-card">
             <img src={photo} alt="" />
             <h2>{name}</h2>
-            <p>Username: {username}</p>     
+            <p>Username: {username}</p>
             <p>Email: {email}</p>
             <p>About: {about}</p>
         </div>
         <div>
             <p>age: {age}</p>
             <p>Address: {address}</p>
+            <p>mobility: {mobility}</p>
+            <p>diponibility: {disponibility}</p>
             <ul>
-                {hobbies.map((i, el)=>(<li key={i}>{el}</li>))}
+                {hobbies.map((i, el) => (<li key={i}>{el}</li>))}
             </ul>
         </div>
     </>
     );
 };
- 
+
 export default ProfileCard;
 
